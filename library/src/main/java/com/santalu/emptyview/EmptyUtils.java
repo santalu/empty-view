5915d208c237b3727bd3deeb0014dfe1b3e61a98
santalu <ftsantalu@gmail.com>
2018-04-10 15:55:02 +0300

    Add gravity option Add styles for each child view to make it more customizable

--- library/src/main/java/com/santalu/emptyview/EmptyUtils.java
+++ library/src/main/java/com/santalu/emptyview/EmptyUtils.java
@@ -4,20 +4,16 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.support.annotation.ColorRes;
 import android.support.annotation.DimenRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.FontRes;
-import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.annotation.TransitionRes;
 import android.support.transition.Transition;
 import android.support.transition.TransitionInflater;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
-import android.text.Html;
-import android.text.TextUtils;
 import android.util.TypedValue;
 import android.widget.TextView;
 
@@ -50,16 +46,6 @@ final class EmptyUtils {
 
   static Transition getTransition(Context context, @TransitionRes int id) {
     return id <= 0 ? null : TransitionInflater.from(context).inflateTransition(id);
-  }
-
-  static String fromHtml(@Nullable String text) {
-    if (TextUtils.isEmpty(text)) {
-      return null;
-    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-      return Html.fromHtml(text, Html.FROM_HTML_MODE_COMPACT).toString();
-    } else {
-      return Html.fromHtml(text).toString();
-    }
   }
 
   static void setTextSize(TextView textView, float textSize) {
